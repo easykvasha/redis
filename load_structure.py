8c66cd8ce80d0a12c96d7df9ef2bd4aed692f239
easykvasha <61687166+easykvasha@users.noreply.github.com>
2022-04-16 18:30:44 +0300

    Update load_structure.py

--- load_structure.py
+++ load_structure.py
@@ -4,7 +4,7 @@ import time
 
 if __name__ == '__main__':
 
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r = redis.StrictRedis(host='localhost', port=6379, db=2)
 
     print('Load keys.')
 
